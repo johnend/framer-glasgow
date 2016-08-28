e1b37a2ca8a99a5fda35db70d987fffc8229ab89
John Enderby <john.enderby.1987@gmail.com>
2016-08-28 21:49:49 +0100

    basic copy written, header section adjusted, some layout included for copy

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var coffee = require('gulp-coffee');
 var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var del = require('del');
+var runSequence = require('run-sequence');
 
 
 gulp.task('sass', function(){
@@ -28,9 +34,39 @@ gulp.task('coffee', function () {
         .pipe(gulp.dest('app/js'));
 });
 
+gulp.task('useref', function () {
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('fonts', function () {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('clean:dist', function () {
+  return del.sync('dist');
+});
+
 gulp.task('watch', ['sass', 'coffee', 'browserSync'], function() {
    gulp.watch('app/coffee/*.coffee', ['coffee']);
    gulp.watch('app/scss/*.scss', ['sass']);
    gulp.watch('app/**/*.html', browserSync.reload);
    gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('build', function(callback){
+  runSequence('clean:dist', 
+    ['sass', 'coffee', 'useref', 'fonts'], 
+    callback
+  );
+});
+
+gulp.task('default', function(callback){
+  runSequence(['sass', 'coffee', 'browserSync', 'watch'],
+    callback
+    );
 });
